5f914701af0aafabb16ce764118166a90fe38eb1
lorennavitoria <lorennalovi@gmail.com>
2024-04-25 22:28:14 -0400

    teste

--- src/main/java/edu/ifmt/doacoes/DoacoesApplication.java
+++ src/main/java/edu/ifmt/doacoes/DoacoesApplication.java
@@ -1,14 +1,8 @@
 package edu.ifmt.doacoes;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.JpaRepository;
 
-import edu.ifmt.doacoes.controller.AdministradorController;
-import edu.ifmt.doacoes.model.Administrador;
-import edu.ifmt.doacoes.repositories.AdministradorRepository;
-import jakarta.persistence.EntityManager;
 
 @SpringBootApplication
 public class DoacoesApplication {
